22d3a19391942c4205446b3bdfe7dcbc75d64c58
NNQA <anhla12h@gmail.com>
2023-11-17 10:32:02 +0700

    refator: refactor model post

--- src/Human.Domain/Models/Post.cs
+++ src/Human.Domain/Models/Post.cs
@@ -9,4 +9,5 @@ public record class Post : IAggregationRoot
     public Instant UpdatedTime { get; set; }
     public string Subject { get; set; } = null!;
     public Message InitialMessage { get; set; } = null!;
+    public ICollection<Tag> Tags { get; set; } = new HashSet<Tag>();
 }
